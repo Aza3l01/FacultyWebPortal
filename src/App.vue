913ae02df716b58002a617986723ba47aa5a458c
Aza3l01 <aza3l0309@gmail.com>
2024-03-05 15:12:45 +0530

    modified

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,18 @@ import Educational from './components/Educational.vue';
 import Experience from './components/Experience.vue';
 import Research from './components/Research.vue';
 import Academic from './components/Academic.vue';
+import Patent from './components/Patent.vue';
+import Responsibilities from './components/Responsibilities.vue';
+import Dresponsibilities from './components/Dresponsibilities.vue';
+import Aresponsibilities from './components/Aresponsibilities.vue';
+import Laboratory from './components/Laboratory.vue';
+import Mou from './components/Mou.vue';
+import Talks from './components/Talks.vue';
+import Training from './components/Training.vue';
+import Memberships from './components/Memberships.vue';
+import Reviewer from './components/Reviewer.vue';
+import Awards from './components/Awards.vue';
+import Links from './components/Links.vue';
 </script>
 
 <template>
@@ -17,6 +29,18 @@ import Academic from './components/Academic.vue';
     <Experience />
     <Research />
     <Academic />
+    <Patent />
+    <Responsibilities />
+    <Dresponsibilities />
+    <Aresponsibilities />
+    <Laboratory />
+    <Mou />
+    <Talks />
+    <Training />
+    <Memberships />
+    <Reviewer />
+    <Awards />
+    <Links />
   </div>
 </template>
 
